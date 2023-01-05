9ce15c93b686a82d74f22c7420375b71705a8fb3
Rodrigo Arias <rodrigoariasramirez@gmail.com>
2023-01-05 14:27:08 -0300

    Cambio cifra de resultados y algunos detalles

--- js/script.js
+++ js/script.js
@@ -6,11 +6,11 @@ function convertir() {
     var euro = 0.024;
     if (document.getElementById("uno").checked){
         resultado = valore / dolar;
-        alert("El cambio de Dolares a Pesos Uruguayos es: $" + resultado);
+        alert("El cambio de Dolares a Pesos Uruguayos es: $" + resultado.toFixed(2));
         }
     else if (document.getElementById("dos").checked){
         resultado = valore / euro;
-        alert("El cambio Euros a Pesos Uruguayos es:" + resultado);
+        alert("El cambio Euros a Pesos Uruguayos es: $" + resultado.toFixed(2));
 
     }
     else{
