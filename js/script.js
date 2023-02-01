7d435a6033b937e8e720fd5e0e930207ce748c67
Rodrigo <rodrigoariasramirez@gmail.com>
2023-02-01 03:54:12 -0300

    Ordenando código

--- js/script.js
+++ js/script.js
@@ -1,19 +1,16 @@
-type="text/javascript"
+type = "text/javascript";
 function convertir() {
-    let valore = parseInt(document.getElementById("valor").value);
-    let resultado = 0;
-    let dolar = 0.025;
-    let euro = 0.024;
-    if (document.getElementById("uno").checked){
-        resultado = valore / dolar;
-        swal("El cambio de Dolares a Pesos Uruguayos es: $" + resultado.toFixed(2));
-        }
-    else if (document.getElementById("dos").checked){
-        resultado = valore / euro;
-        swal("El cambio de Euros a Pesos Uruguayos es: $" + resultado.toFixed (2));
-
-    }
-    else{
-        swal("Tienes que ingresar el monto")
-    }    
+  let valore = parseInt(document.getElementById("valor").value);
+  let resultado = 0;
+  let dolar = 0.025;
+  let euro = 0.024;
+  if (document.getElementById("uno").checked) {
+    resultado = valore / dolar;
+    swal("El cambio de Dolares a Pesos Uruguayos es: $" + resultado.toFixed(2));
+  } else if (document.getElementById("dos").checked) {
+    resultado = valore / euro;
+    swal("El cambio de Euros a Pesos Uruguayos es: $" + resultado.toFixed(2));
+  } else {
+    swal("Tienes que ingresar el monto");
+  }
 }
